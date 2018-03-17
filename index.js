16ca5d8242cc3822047a327645b7ebc0d63699c2
javi <enriq018coding@gmail.com>
2018-03-16 23:09:06 -0700

    add reddit random quote bot response test

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { token } = require('./config.json');
+const axios = require('axios');
 
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 client.on('ready', () => {
     console.log('Ready!');
 });
 
-
 client.on('message', message => {
-  console.log(message.content);
-  if (message.content === '!buildBot') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.');
+  //message content is what the bot will look for
+  if (message.content === '!') {
+    //api request
+    axios.get('http://www.reddit.com/r/quotes/top/.json?count=5')
+    .then((response) => {
+      let info = response.data.data.children[getRandomInt(0,6)].data.title;
+      //bot message to the channel
+      message.channel.send(JSON.stringify(info))
+    })
+    .catch((error) => {
+      message.channel.send('Damn Chin...Something didn\'t work');
+    });
   }
 });
 
